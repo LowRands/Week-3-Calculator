fb81b30c0196350d8ed732a3080626f9cdb779d5
EthanLourens <G00467054@atu.ie>
2025-10-01 18:02:14 +0100

    Adding new method called power and modulo

--- src/main/java/ie/atu/week3/Calculator.java
+++ src/main/java/ie/atu/week3/Calculator.java
@@ -1,17 +1,37 @@
 package ie.atu.week3;
 
 public class Calculator {
-    public int add(int firstNum, int secondNum) {
+    public double add(double firstNum, double secondNum) {
         return firstNum + secondNum;
-
     }
-    public int subtract(int firstNum, int secondNum) {
+
+    public double subtract(double firstNum, double secondNum) {
         return firstNum - secondNum;
     }
-    public int multiply(int firstNum, int secondNum) {
+
+    public double multiply(double firstNum, double secondNum) {
         return firstNum * secondNum;
     }
-    public int divide(int firstNum, int secondNum) {
-        return firstNum / secondNum;
+
+    public double divide(double firstNum, double secondNum) {
+        if (secondNum == 0) {
+            System.out.println("Error");
+            return 0;
+        } else {
+            return firstNum / secondNum;
+        }
+    }
+
+    public double power(double base, double exponent) {
+        if (exponent == 0) {
+            return 1;
+        } else if (exponent == 1) {
+            return base;
+        }else{
+            return Math.pow(base, exponent);
+        }
+    }
+    public  double mod(double a, double b) {
+        return a % b;
     }
 }
